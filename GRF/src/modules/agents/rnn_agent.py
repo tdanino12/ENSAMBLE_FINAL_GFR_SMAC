79305d8c6de6919b1d8187b3da4190460efd6ec1
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-22 23:53:12 +0300

    Update rnn_agent.py

--- GRF/src/modules/agents/rnn_agent.py
+++ GRF/src/modules/agents/rnn_agent.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch as th
+import numpy as np
 
 class RNNAgent(nn.Module):
     def __init__(self, input_shape, args):
@@ -144,6 +145,53 @@ class RNNAgent(nn.Module):
         return (q+q_2+q_3+q_4+q_5+q_6+q_7)/th.tensor(7) ,q,q_2,q_3,q_4,q_5,q_6,q_7, h,h_2,h_3,h_4,h_5,h_6,h_7
 
 '''
+Soft modularization ןinit function
+'''
+
+def _fanin_init(tensor, alpha = 0):
+    size = tensor.size()
+    if len(size) == 2:
+        fan_in = size[0]
+    elif len(size) > 2:
+        fan_in = np.prod(size[1:])
+    else:
+        raise Exception("Shape must be have dimension at least 2.")
+    # bound = 1. / np.sqrt(fan_in)
+    bound = np.sqrt( 1. / ( (1 + alpha * alpha ) * fan_in) )
+    return tensor.data.uniform_(-bound, bound)
+
+def _uniform_init(tensor, param=3e-3):
+    return tensor.data.uniform_(-param, param)
+
+def _constant_bias_init(tensor, constant = 0.1):
+    tensor.data.fill_( constant )
+
+def _normal_init(tensor, mean=0, std =1e-3):
+    return tensor.data.normal_(mean,std)
+
+def layer_init(layer, weight_init = _fanin_init, bias_init = _constant_bias_init ):
+    weight_init(layer.weight)
+    bias_init(layer.bias)
+
+def basic_init(layer):
+    layer_init(layer, weight_init = _fanin_init, bias_init = _constant_bias_init)
+
+def uniform_init(layer):
+    layer_init(layer, weight_init = _uniform_init, bias_init = _uniform_init )
+
+def normal_init(layer):
+    layer_init(layer, weight_init = _normal_init, bias_init = _normal_init)
+    
+def _orthogonal_init(tensor, gain = np.sqrt(2)):
+    nn.init.orthogonal_(tensor, gain = gain)
+
+def orthogonal_init(layer, scale = np.sqrt(2), constant = 0 ):
+    layer_init(
+        layer,
+        weight_init= lambda x:_orthogonal_init(x, gain=scale),
+        bias_init=lambda x: _constant_bias_init(x, 0))
+
+'''
 Soft modularization network
 '''
 class ModularGatedCascadeCondNet(nn.Module):
@@ -162,8 +210,8 @@ class ModularGatedCascadeCondNet(nn.Module):
             add_bn = True,
             pre_softmax = False,
             cond_ob = True,
-            module_hidden_init_func = init.basic_init,
-            last_init_func = init.uniform_init,
+            module_hidden_init_func = basic_init,
+            last_init_func = uniform_init,
             activation_func = F.relu,
              **kwargs ):
 
