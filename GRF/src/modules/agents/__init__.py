5f099add58cd07b6f0cce530daa84027caa818b4
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-23 16:15:38 +0300

    Update __init__.py

--- GRF/src/modules/agents/__init__.py
+++ GRF/src/modules/agents/__init__.py
@@ -1,6 +1,8 @@
 REGISTRY = {}
 
 from .rnn_agent import RNNAgent
+from .rnn_agent import ModularGatedCascadeCondNet
 from .grnn_agent import GRNNAgent
 REGISTRY["rnn"] = RNNAgent
+REGISTRY["soft"] = ModularGatedCascadeCondNet
 REGISTRY["grnn"] = GRNNAgent
