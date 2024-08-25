28e2078a74b000b66e3067b29662d0a5a7fe2772
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-25 16:33:24 +0300

    Update depth_route_net.py

--- GRF/src/modules/agents/depth_route_net.py
+++ GRF/src/modules/agents/depth_route_net.py
@@ -4,6 +4,51 @@ import torch.nn as nn
 import torch.nn.functional as F
 import copy
 
+
+class MLPBase(nn.Module):
+    def __init__(self, input_shape, hidden_shapes, activation_func=F.relu, init_func = init.basic_init, last_activation_func = None ):
+        super().__init__()
+        
+        self.activation_func = activation_func
+        self.fcs = []
+        if last_activation_func is not None:
+            self.last_activation_func = last_activation_func
+        else:
+            self.last_activation_func = activation_func
+        input_shape = np.prod(input_shape)
+
+        self.output_shape = input_shape
+        for i, next_shape in enumerate( hidden_shapes ):
+            fc = nn.Linear(input_shape, next_shape)
+            init_func(fc)
+            self.fcs.append(fc)
+            # set attr for pytorch to track parameters( device )
+            self.__setattr__("fc{}".format(i), fc)
+
+            input_shape = next_shape
+            self.output_shape = next_shape
+
+    def forward(self, x):
+
+        out = x
+        for fc in self.fcs[:-1]:
+            out = fc(out)
+            out = self.activation_func(out)
+        out = self.fcs[-1](out)
+        out = self.last_activation_func(out)
+        return out
+
+def calc_next_shape(input_shape, conv_info):
+    """
+    take input shape per-layer conv-info as input
+    """
+    out_channels, kernel_size, stride, padding = conv_info
+    c, h, w = input_shape
+    # for padding, dilation, kernel_size, stride in conv_info:
+    h = int((h + 2*padding[0] - ( kernel_size[0] - 1 ) - 1 ) / stride[0] + 1)
+    w = int((w + 2*padding[1] - ( kernel_size[1] - 1 ) - 1 ) / stride[1] + 1)
+    return (out_channels, h, w )
+
 def _fanin_init(tensor, alpha = 0):
     size = tensor.size()
     if len(size) == 2:
@@ -224,7 +269,6 @@ class DepthRouteNet(nn.Module):
             self,
             task_num,
             output_shape,
-            base_type,
             input_shape,
             hidden_shapes,
             em_hidden_shapes,
@@ -251,14 +295,14 @@ class DepthRouteNet(nn.Module):
         self.base_input_shape = np.sum(input_shape[:-1])
         self.em_input_shape = input_shape[-1]
 
-        self.base = base_type( 
+        self.base = MLPBase( 
                         last_activation_func = null_activation,
                         input_shape = self.base_input_shape,
                         activation_func = activation_func,
                         hidden_shapes = hidden_shapes,
                         **kwargs)
 
-        self.em_base = base_type(
+        self.em_base = MLPBase(
                         last_activation_func = null_activation,
                         input_shape = self.em_input_shape,
                         activation_func = activation_func,
