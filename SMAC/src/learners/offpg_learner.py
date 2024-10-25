fce998790d29e19224e308ef607d006d9c6caa49
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-10-25 17:16:01 +0300

    Update offpg_learner.py

--- SMAC/src/learners/offpg_learner.py
+++ SMAC/src/learners/offpg_learner.py
@@ -174,16 +174,19 @@ class OffPGLearner:
         pi = mac_out.view(-1, self.n_actions)
         baseline = th.sum(mac_out * q_vals, dim=-1).view(-1).detach()
 
+
+        coma_loss = -(self.mixer.forward(baseline, states,False,None)* mask).sum() / mask.sum()
+        
         # Calculate policy grad with mask
-        pi_taken = th.gather(pi, dim=1, index=actions.reshape(-1, 1)).squeeze(1)
-        pi_taken[mask == 0] = 1.0
-        log_pi_taken = th.log(pi_taken)
-        coe = self.mixer.k(states).view(-1)
+        #pi_taken = th.gather(pi, dim=1, index=actions.reshape(-1, 1)).squeeze(1)
+        #pi_taken[mask == 0] = 1.0
+        #log_pi_taken = th.log(pi_taken)
+        #coe = self.mixer.k(states).view(-1)
         #entropy = -(pi*log_pi_taken)
                     
-        advantages = (q_taken.view(-1) - baseline).detach()
+        #advantages = (q_taken.view(-1) - baseline).detach()
 
-        coma_loss = - ((coe * (advantages)* log_pi_taken ) * mask).sum() / mask.sum()
+        #coma_loss = - ((coe * (advantages)* log_pi_taken ) * mask).sum() / mask.sum()
         #coma_loss = - ((coe * (advantages)* log_pi_taken+total_div ) * mask).sum() / mask.sum()
         # Optimise agents
         self.agent_optimiser.zero_grad()
